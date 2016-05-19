83374f88eb35706ede7da584a5a57343e219bcbd
Guillaume Walck <gwalck@techfak.uni-bielefeld.de>
2016-05-20 00:07:13 +0200

    Moved CMakeLists.txt and python scripts out of source to satisfy catkin and added all necessary installs

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['m3ros_control'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    scripts=['scripts/state_manager.py']
 )
 
 setup(**d)
